f0694c4ec9681febdeaedfdb22a33236693228b2
Sobolev Roman <39559342+shaxqq@users.noreply.github.com>
2021-09-13 13:18:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8000;
 app.use(express.static('build'));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () =>
